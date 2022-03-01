bf89312b5404ae71a40b1cffceffacca6e73a5b1
ankit711 <itsankitsingh711@edureka.co>
2022-03-01 16:59:20 +0530

    firstcommit

---
+++ calculator.js
@@ -0,0 +1,17 @@
+function valueButton(e){
+    calculatorform.screen.value += e.value;
+}
+
+function Clear(){
+    calculatorform.screen.value = null;
+}
+
+function Calculate(){
+    calculatorform.screen.value = eval( calculatorform.screen.value);
+}
+
+function changemode() {
+    var body = document.body
+    body.classList.toggle("darkmode")
+}
+
